624226f55f338c17432f40a42e74f34aff29c0a1
Blisse225 <sankaraarouna216@gmail.com>
2026-07-31 03:10:37 +0100

    Update produits.js

--- produits.js
+++ produits.js
@@ -8,9 +8,16 @@ for (let i = 1; i <= 45; i++) {
         prix: 4000,
         image: "drap2place (" + i + ").jpg",
         categorie: "Draps",
-        description: "Drap 2 places confortable et élégant, parfait pour votre chambre."
+        description: "Drap 2 places confortable et élégant, parfait pour votre chambre avec 2 coussins."
     });
-
+{
+    id: i,
+    nom: "Drap 2 places",
+    prix: 8000,
+    image: "drap2places (" + i + ").jpg",
+    categorie: "Draps",
+    description: "Drap 2 places confortable et élégant pour votre chambre avec 2 coussins."
+}
 }
 
 
